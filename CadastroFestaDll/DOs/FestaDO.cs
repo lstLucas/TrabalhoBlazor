2add76eba9a8ce87216a3f0f759718aae0796b6c
lstLucas <lucas.s.teixeira@viannasempre.com.br>
2023-06-22 09:56:55 -0300

    Alterando as páginas e modificando o FestaDO

--- CadastroFestaDll/DOs/FestaDO.cs
+++ CadastroFestaDll/DOs/FestaDO.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CadastroFestaDll.DOs
 {
     public class FestaDO : BaseDO
     {
+        [Required]
+        [StringLength(100, ErrorMessage="O nome da festa deve ser inferior a 100 caracteres.")]
+        public string Nome { get; set; } = "";
+
+        [StringLength(80, ErrorMessage="O tema deve ser inferior a 80 caracteres")]
+        public string Tema { get; set; } = "";
+
+        public DateTime Data { get; set; }
     }
 }
